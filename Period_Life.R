c2efcd09e5d6297f3c1ce5437f657984396bcd99
B-Deprez <bruno.r.deprez@gmail.com>
2022-08-21 11:08:08 +0200

    removed demography library due to problems

--- Period_Life.R
+++ Period_Life.R
@@ -1,8 +1,6 @@
-
 # Loading packages and setting directory
 library(tidyverse)
 library(dplyr)
-library(demography)
 library(ggplot2)
 library(gridExtra)
 
@@ -14,14 +12,6 @@ dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(dir)
 
 ## Reading in database
-# This is used to have an accurate view on population size
-User = "***"
-pw = "***"
-df = hmd.mx("BEL", User , pw , "Belgium")
-
-#Table with data from online
-mortality_table <- df$pop$total
-
 #Overview of mortality data from txt-file downloaded from HMD
 Belgian_mortality <- readr::read_delim(
   file = "Belgium.txt",
